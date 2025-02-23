697523ee68656dc81ead75527aef809c8762777b
Yana Pavlova <yn-pvlv@yandex.ru>
2025-02-23 19:48:55 +0300

    feat: implement removeProperties function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -54,8 +54,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = { ...obj };
+  keys.forEach((key) => {
+    delete newObj[key];
+  });
+  return newObj;
 }
 
 /**
