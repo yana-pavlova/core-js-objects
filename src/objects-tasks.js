8718db30fd5a32f1ca463585d46a0d33c29d6d15
Yana Pavlova <yn-pvlv@yandex.ru>
2025-02-23 21:27:35 +0300

    feat: implement makeWord function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -125,8 +125,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const res = [];
+  Object.keys(lettersObject).forEach((letter) => {
+    lettersObject[letter].forEach((position) => {
+      res[position] = letter;
+    });
+  });
+
+  return res.join('');
 }
 
 /**
