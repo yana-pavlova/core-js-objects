05cf7e778ec8cb858927f6af0e9cc25f43eb5292
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-03 15:45:33 +0300

    feat: implement fromJSON function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -215,8 +215,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+
+  return obj;
 }
 
 /**
